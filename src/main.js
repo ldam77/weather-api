f371eb7286359f4cc85054339744e2f3156f1d5a
Anousone Kaseumsouk, Jean Jia and Lan Dam <anousone@>
2018-08-07 11:13:07 -0700

    ajax and xmlhttprequest for project.

---
+++ src/main.js
@@ -0,0 +1,46 @@
+// $(document).ready(function() {
+//   $('#weatherLocation').click(function() {
+//     let city = $('#location').val();
+//     $('#location').val("");
+//     $.ajax({
+//       url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c49fc54dde82cf66d2795d8e9ce406ba`,
+//       type: 'GET',
+//       data: {
+//         format: 'json'
+//       },
+//       success: function(response) {
+//         $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+//         $('.showTemp').text(`The temperature in Celcius is ${response.main.temp -273}.`);
+//         $('.showFarenheit').text(`The temperature in Farenheit is ${(response.main.temp -273)*9/4 + 32}.`);
+//       },
+//       error: function() {
+//         $('#errors').text("There was an error processing your request. Please try again.");
+//       }
+//     });
+//   });
+// });
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    let city = $('#location').val();
+    $('#location').val("");
+
+    let request = new XMLHttpRequest();
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+    let getElements = function(response) {
+      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+    }
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        let response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    }
+
+    request.open("GET", url, true);
+    request.send();
+
+  });
+});
